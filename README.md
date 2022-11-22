2b7f92699ce055e6168bac113fa76f75e29d5599
Micplus <Micplus@github.com>
2022-11-23 02:11:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# wrzap
+
+对zap日志库的包装，通过导入包的方式使logger全局唯一
+
+- wrzap = wrap zap
+- 无糖Logger
+
+参考： https://github.com/bigwhite/experiments/tree/master/uber-zap-advanced-usage
+
+## Dependency
+
+go.uber.org/zap v1.23.0
+
+## Usage
+
+```go
+package main
+
+import log "github.com/micplus/wrzap"
+
+func main() {
+    defer log.Sync()
+    log.Info("hello, zap",
+        log.String("user", "myname"),
+        log.Int("count", 1),
+    )
+}
+```
---
+++ wrzap.go
@@ -0,0 +1,161 @@
+// wrap zap logger
+package wrzap
+
+import (
+	"io"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Level = zapcore.Level
+
+const (
+	InfoLevel   Level = zap.InfoLevel   // 0
+	WarnLevel   Level = zap.WarnLevel   // 1
+	ErrorLevel  Level = zap.ErrorLevel  // 2
+	DPanicLevel Level = zap.DPanicLevel // 3
+	PanicLevel  Level = zap.PanicLevel  // 4
+	FatalLevel  Level = zap.FatalLevel  // 5
+	DebugLevel  Level = zap.DebugLevel  // -1
+)
+
+type Field = zap.Field
+
+func (l *Logger) Debug(msg string, fields ...Field) {
+	l.l.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...Field) {
+	l.l.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...Field) {
+	l.l.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...Field) {
+	l.l.Error(msg, fields...)
+}
+func (l *Logger) DPanic(msg string, fields ...Field) {
+	l.l.DPanic(msg, fields...)
+}
+func (l *Logger) Panic(msg string, fields ...Field) {
+	l.l.Panic(msg, fields...)
+}
+func (l *Logger) Fatal(msg string, fields ...Field) {
+	l.l.Fatal(msg, fields...)
+}
+
+var (
+	Skip        = zap.Skip
+	Binary      = zap.Binary
+	Bool        = zap.Bool
+	Boolp       = zap.Boolp
+	ByteString  = zap.ByteString
+	Complex128  = zap.Complex128
+	Complex128p = zap.Complex128p
+	Complex64   = zap.Complex64
+	Complex64p  = zap.Complex64p
+	Float64     = zap.Float64
+	Float64p    = zap.Float64p
+	Float32     = zap.Float32
+	Float32p    = zap.Float32p
+	Int         = zap.Int
+	Intp        = zap.Intp
+	Int64       = zap.Int64
+	Int64p      = zap.Int64p
+	Int32       = zap.Int32
+	Int32p      = zap.Int32p
+	Int16       = zap.Int16
+	Int16p      = zap.Int16p
+	Int8        = zap.Int8
+	Int8p       = zap.Int8p
+	String      = zap.String
+	Stringp     = zap.Stringp
+	Uint        = zap.Uint
+	Uintp       = zap.Uintp
+	Uint64      = zap.Uint64
+	Uint64p     = zap.Uint64p
+	Uint32      = zap.Uint32
+	Uint32p     = zap.Uint32p
+	Uint16      = zap.Uint16
+	Uint16p     = zap.Uint16p
+	Uint8       = zap.Uint8
+	Uint8p      = zap.Uint8p
+	Uintptr     = zap.Uintptr
+	Uintptrp    = zap.Uintptrp
+	Reflect     = zap.Reflect
+	Namespace   = zap.Namespace
+	Stringer    = zap.Stringer
+	Time        = zap.Time
+	Timep       = zap.Timep
+	Stack       = zap.Stack
+	StackSkip   = zap.StackSkip
+	Duration    = zap.Duration
+	Durationp   = zap.Durationp
+	Any         = zap.Any
+
+	Info   = logger.Info
+	Warn   = logger.Warn
+	Error  = logger.Error
+	DPanic = logger.DPanic
+	Panic  = logger.Panic
+	Fatal  = logger.Fatal
+	Debug  = logger.Debug
+)
+
+// 创建新的logger替换包内的logger；
+// 这个行为不是并发安全的(换logger时没写完)，可以在程序初始化的时候配置它
+func ResetDefault(l *Logger) {
+	logger = l
+	Info = logger.Info
+	Warn = logger.Warn
+	Error = logger.Error
+	DPanic = logger.DPanic
+	Panic = logger.Panic
+	Fatal = logger.Fatal
+	Debug = logger.Debug
+}
+
+type Logger struct {
+	// zap库保证并发安全
+	l     *zap.Logger
+	level Level
+}
+
+// 包级唯一
+var logger = New(os.Stderr, InfoLevel)
+
+func Default() *Logger {
+	return logger
+}
+
+func New(writer io.Writer, level Level) *Logger {
+	if writer == nil {
+		panic("nil log writer")
+	}
+	cfg := zap.NewProductionConfig()
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(cfg.EncoderConfig),
+		zapcore.AddSync(writer),
+		zapcore.Level(level),
+	)
+	logger := &Logger{
+		l:     zap.New(core),
+		level: level,
+	}
+	return logger
+}
+
+func (l *Logger) Sync() error {
+	return l.l.Sync()
+}
+
+func Sync() error {
+	if logger != nil {
+		return logger.Sync()
+	}
+	return nil
+}
